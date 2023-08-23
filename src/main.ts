12b0820bfd4dd59b6168f2a2f6c7916efc523622
YuuuJeong <dbwjdgh03@ajou.ac.kr>
2023-08-23 20:06:04 +0900

    feat: swagger 적용 및 config파일 수정

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,38 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api', {
     exclude: ['/'],
   });
+
+  const user = app.get<ConfigService>(ConfigService).get('SWAGGER_USER');
+  const pw = app.get<ConfigService>(ConfigService).get('SWAGGER_PASSWORD');
+  app.use(
+    ['/api/docs'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [user]: pw,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('TravelMate Swagger')
+    .setDescription('캡스톤 디자인 TravelMate BE 스웨거입니다.')
+    .setVersion('1.0.0')
+    .addTag('swagger')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   const port = app.get<ConfigService>(ConfigService).get('SERVER_PORT');
-  console.log(port);
+
   await app.listen(port);
 }
 bootstrap();
