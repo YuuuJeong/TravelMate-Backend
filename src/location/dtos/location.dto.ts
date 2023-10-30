5dcf3bd757e8125f7bda3ba5b6761a4783a271a1
YuuuJeong <dbwjdgh03@ajou.ac.kr>
2023-10-30 19:35:30 +0900

    feat: 스키마 변경

--- src/location/dtos/location.dto.ts
+++ src/location/dtos/location.dto.ts
@@ -1,13 +1,13 @@
 import { Exclude, Expose } from 'class-transformer';
-import { Prisma } from '@prisma/client';
+import { Decimal } from '@prisma/client/runtime/library';
 
 export class LocationDto {
   @Exclude()
   id: number;
 
   @Expose()
-  latitude: Prisma.Decimal;
+  latitude: Decimal;
 
   @Expose()
-  longitude: Prisma.Decimal;
+  longitude: Decimal;
 }
